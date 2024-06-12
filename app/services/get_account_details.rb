e9c49208b93d881c38619f7c11dfec12bb65fe6d
Angel90226 <angel.chung@iss.nthu.edu.tw>
2024-06-12 17:25:50 +0800

    add api auth token to account details page

---
+++ app/services/get_account_details.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'http'
+
+class ScanChat
+  # Returns all details of an account
+  class GetAccountDetails
+    # Error for accounts that cannot be seen
+    class InvalidAccount < StandardError
+      def message
+        'You are not authorized to see details of that account'
+      end
+    end
+
+    def initialize(config)
+      @config = config
+    end
+
+    def call(current_account, username)
+      response = HTTP.auth("Bearer #{current_account.auth_token}")
+                     .get("#{@config.API_URL}/accounts/#{username}")
+      raise InvalidAccount if response.code != 200
+
+      data = JSON.parse(response)['data']
+      account_details = data['attributes']['account']
+      auth_token = data['attributes']['auth_token']
+      ScanChat::Account.new(account_details, auth_token)
+    end
+  end
+end
